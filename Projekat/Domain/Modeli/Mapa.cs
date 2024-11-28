60d557acbffe92e5f04a76a347324d52ea2f1405
Stefan Vlašković <PC@VLASKO>
2024-11-28 14:40:37 +0100

    Kreiranje klase Mapa

---
+++ Projekat/Domain/Modeli/Mapa.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Enumeracija;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Modeli
+{
+    public class Mapa
+    {
+        public string NazivMape {  get; set; } = string.Empty;
+        public Tip_Mape Tip_Mape { get; set; }
+        public int MaksimalanBrojIgraca { get; set; } = 0;
+        public string NazivCrvenih {  get; set; } = string.Empty;   
+        public string NazivPlavih {  get; set; } = string.Empty;
+        public int BrojPomocnih { get; set; } = 0;
+
+        public Mapa() { }
+
+        public Mapa (string nazivMape, Tip_Mape tip_Mape, int maksimalanBrojIgraca, string nazivCrvenih, string nazivPlavih, int brojPomocnih)
+        {
+            NazivMape = nazivMape;
+            Tip_Mape = tip_Mape;
+            MaksimalanBrojIgraca = maksimalanBrojIgraca;
+            NazivCrvenih = nazivCrvenih;
+            NazivPlavih = nazivPlavih;
+            BrojPomocnih = brojPomocnih;
+        }
+    }
+}
--- Projekat/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Projekat/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9850b4bd47b867cf73b103d4e5625faf0cd1b7cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38de26a8612b72b35ed1dc5ca0257367cb712fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
